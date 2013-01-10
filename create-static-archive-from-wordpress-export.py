5046642cbac4588b9de1f9becb1c38720d7d544f
Aral Balkan <aral@aralbalkan.com>
2013-01-10 20:48:04 +0000

    Moved the post HTML into its own template file.

--- create-static-archive-from-wordpress-export.py
+++ create-static-archive-from-wordpress-export.py
@@ -45,7 +45,12 @@ indexPostListHTML = ''
 
 # post = wp.postsPublished[0]
 
-counter = 0
+# counter = 0
+
+# Load the post template
+postTemplateFile = open('post-template.html', 'r')
+postTemplate = unicode(postTemplateFile.read(), 'utf_8')
+postTemplateFile.close()
 
 for post in wp.postsPublished:
 
@@ -172,115 +177,25 @@ for post in wp.postsPublished:
 
     post['content'] = massagedContent
 
-    #
-    # Write out the post.
-    #
-
     # Time since the post was written
     dateOfPost = datetime.strptime(post['date'], '%Y-%m-%d %H:%M:%S')
     ageOfPost = timesince(dateOfPost)
 
-    html = u"""
-    <html>
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width">
-            <link rel="icon" href="/favicon.ico">
-            <link rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">
-            <link rel="stylesheet" href="/css/reset.css">
-            <link rel="stylesheet" href="/css/stylus/styles.css">
-            <link rel="stylesheet" href="/css/stylus/archive/styles.css">
-            <title>Aral Balkan: Historical Archive&#8202;—&#8202;%s</title>
-        </head>
-        <body>
-            <header>
-                <h1>Aral Balkan</h1>
-            </header>
+    #
+    # Create and write out the post.
+    #
 
-            <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/archive">Archive</a></li>
-                    <li><span class="current-page">%s</span></li>
-                </ul>
-            </nav>
+    postTitle = post['title']
+    if postTitle == None:
+        postTitle = 'Untitled'
+        print '\tWarning: post with id %s is missing post title.' % post['id']
 
-            <section id="archiveDisclaimer">
-                <strong>Historical content:</strong> I wrote this article over %s on %s. You are viewing an archived post from my old WordPress blog. The archive contains over 1,500 articles written over a ten year period. The formatting and contents of the posts may not display perfectly.
-            </section>
-
-            <div role="content">
-
-                <h2>%s</h2>
-                <section id="post">
-                %s
-                </section>
-                <!-- Comments -->
-                %s
-            </div>
-            <footer>
-                <section>
-                    <h2>Contact</h2>
-                    <ul>
-                        <li>Twitter: <a rel="me" href="http://twitter.com/aral">@aral</a></li>
-                        <li>Email: <a href="mailto:aral@aralbalkan.com">aral@aralbalkan.com</a></li>
-                    </ul>
-                </section>
-
-                <section id="copyright">
-                    <nav>
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/archive">Archive</a></li>
-                        <li><span class="current-page">%s</span></li>
-                    </ul>
-                </nav>
-                        <small>Copyright © 2012–2013 Aral Balkan. <a href="http://www.flickr.com/photos/cvonposer/6401515437" title="Aral Balkan">Photo</a> courtesy of <a href="http://www.flickr.com/photos/cvonposer/" title="Christina von Poser Photostream">Christina von Poser</a></small>
-                </section>
-            </footer>
-
-            <script type="text/javascript">
-                //
-                // Hack: Safari has a rendering bug and doesn’t render -webkit-font-smoothing
-                // ===== correctly. So, it’s set to subpixel-antialiased by default and I
-                //       turn it off for Chrome as a progressive enhancement. Firefox doesn’t
-                //       support any font smoothing options but Firefox users probably don’t
-                //       have much aesthetic taste to begin with so I doubt they’ll notice.
-                //
-                if (navigator.userAgent.indexOf("Chrome/") != -1) {
-                    var timeTags = document.getElementsByTagName("time");
-                      for (var i=0; i<timeTags.length; i++) {
-                        timeTags[i].style.webkitFontSmoothing = 'none';
-                    }
-                }
-
-                // Hack: Firefox is an ass.
-                if (navigator.userAgent.indexOf("Firefox/") != -1) {
-                    var timeTags = document.getElementsByTagName("time");
-                      for (var i=0; i<timeTags.length; i++) {
-                        timeTags[i].style.paddingTop = '0.125rem';
-                    }
-                }
-
-                // Gaug.es analytics.
-                var _gauges = _gauges || [];
-                (function() {
-                    var t   = document.createElement('script');
-                    t.type  = 'text/javascript';
-                    t.async = true;
-                    t.id    = 'gauges-tracker';
-                    t.setAttribute('data-site-id', '4f0f72dbcb25bc4e6d000009');
-                    t.src = '//secure.gaug.es/track.js';
-                    var s = document.getElementsByTagName('script')[0];
-                    s.parentNode.insertBefore(t, s);
-                })();
-
-            </script>
-        </body>
-    </html>
-    """ % (post['title'], post['title'], ageOfPost, post['date'].split(' ')[0], post['title'], post['content'], commentsUL, post['title'])
-
-    # print html
+    postHTML = postTemplate
+    postHTML = postHTML.replace(u'{{POST_TITLE}}', postTitle)
+    postHTML = postHTML.replace(u'{{POST_AGE}}', ageOfPost)
+    postHTML = postHTML.replace(u'{{POST_DATE}}', post['date'].split(' ')[0])
+    postHTML = postHTML.replace(u'{{POST_CONTENT}}', post['content'])
+    postHTML = postHTML.replace(u'{{COMMENTS}}', commentsUL)
 
     #
     # Create a folder based on the ID and save the HTML in an index.html file.
@@ -291,21 +206,13 @@ for post in wp.postsPublished:
 
     postFilePath = postFolder + '/index.html'
     file = open(postFilePath, 'w')
-    file.write(html.encode('utf8', 'replace'))
+    file.write(postHTML.encode('utf8', 'replace'))
     file.close()
 
     #
     # Add this post to the index post list for the archive’s index.
     #
-    indexListItem = u'\t\t\t\t<li><a href="/' + post['id'] + u'/">'
-
-    if post['title'] != None:
-        indexListItem += post['title']
-    else:
-        indexListItem += 'Untitled'
-        print '\tWarning: post with id %s is missing post title.' % post['id']
-
-    indexListItem += u'</a>'
+    indexListItem = u'\t\t\t\t<li><a href="/%s/">%s</a>' % (post['id'], postTitle)
 
     # Post date may be None, check for this.
     if post['date'] != None:
