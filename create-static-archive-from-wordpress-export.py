010b3205112a0d9c64c2271222662cc19eb5f2d1
Aral Balkan <aral@aralbalkan.com>
2013-01-10 00:44:10 +0000

    Now outputting test post with regular site style and historic post disclaimer.

--- create-static-archive-from-wordpress-export.py
+++ create-static-archive-from-wordpress-export.py
@@ -56,29 +56,112 @@ html = u"""
         <meta name="viewport" content="width=device-width">
         <link rel="icon" href="/favicon.ico">
         <link rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">
+        <link rel="stylesheet" href="/css/reset.css">
+        <link rel="stylesheet" href="/css/stylus/styles.css">
+        <link rel="stylesheet" href="/css/stylus/archive/styles.css">
         <title>Aral Balkan: Historic Archive&#8202;—&#8202;%s</title>
     </head>
     <body>
-        <h1>%s</h1>
-        %s
-        <hr>
-        %s
+        <header>
+            <h1>Aral Balkan</h1>
+        </header>
+
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/archive">Archive</a></li>
+                <li><span class="current-page">%s</span></li>
+            </ul>
+        </nav>
+
+        <section id="archiveDisclaimer">
+            <p><strong>Historic content:</strong> This article was written on %s. You are viewing an archived post written from the old WordPress blog I had at this site. The archive has over 1,500 articles that I wrote over a ten year period. The formatting and contents of the posts may not display perfectly.</p>
+        </section>
+
+        <div role="content">
+
+            <h2>%s</h2>
+            <section id="post">
+            %s
+            </section>
+            <section id="comments">
+            %s
+            </section>
+        </div>
+        <footer>
+            <section>
+                <h2>Contact</h2>
+                <ul>
+                    <li>Twitter: <a rel="me" href="http://twitter.com/aral">@aral</a></li>
+                    <li>Email: <a href="mailto:aral@aralbalkan.com">aral@aralbalkan.com</a></li>
+                </ul>
+            </section>
+
+            <section id="copyright">
+                <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/archive">Archive</a></li>
+                    <li><span class="current-page">%s</span></li>
+                </ul>
+            </nav>
+                    <small>Copyright © 2012–2013 Aral Balkan. <a href="http://www.flickr.com/photos/cvonposer/6401515437" title="Aral Balkan">Photo</a> courtesy of <a href="http://www.flickr.com/photos/cvonposer/" title="Christina von Poser Photostream">Christina von Poser</a></small>
+            </section>
+        </footer>
+
+        <script type="text/javascript">
+            //
+            // Hack: Safari has a rendering bug and doesn’t render -webkit-font-smoothing
+            // ===== correctly. So, it’s set to subpixel-antialiased by default and I
+            //       turn it off for Chrome as a progressive enhancement. Firefox doesn’t
+            //       support any font smoothing options but Firefox users probably don’t
+            //       have much aesthetic taste to begin with so I doubt they’ll notice.
+            //
+            if (navigator.userAgent.indexOf("Chrome/") != -1) {
+                var timeTags = document.getElementsByTagName("time");
+                  for (var i=0; i<timeTags.length; i++) {
+                    timeTags[i].style.webkitFontSmoothing = 'none';
+                }
+            }
+
+            // Hack: Firefox is an ass.
+            if (navigator.userAgent.indexOf("Firefox/") != -1) {
+                var timeTags = document.getElementsByTagName("time");
+                  for (var i=0; i<timeTags.length; i++) {
+                    timeTags[i].style.paddingTop = '0.125rem';
+                }
+            }
+
+            // Gaug.es analytics.
+            var _gauges = _gauges || [];
+            (function() {
+                var t   = document.createElement('script');
+                t.type  = 'text/javascript';
+                t.async = true;
+                t.id    = 'gauges-tracker';
+                t.setAttribute('data-site-id', '4f0f72dbcb25bc4e6d000009');
+                t.src = '//secure.gaug.es/track.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(t, s);
+            })();
+
+        </script>
     </body>
 </html>
-""" % (post['title'], post['title'], post['content'], commentsUL)
+""" % (post['title'], post['title'], post['date'], post['title'], post['content'], commentsUL, post['title'])
 
-print html
+# print html
 
 #
 # Create a folder based on the ID and save the HTML in an index.html file.
 #
-postFolder = post['id']
+postFolder = 'build/' + post['id']
 if not os.path.exists(postFolder):
     os.makedirs(postFolder)
 
 postFilePath = postFolder + '/index.html'
 file = open(postFilePath, 'w')
-file.write(html)
+file.write(html.encode('utf8', 'replace'))
 file.close()
 
 
